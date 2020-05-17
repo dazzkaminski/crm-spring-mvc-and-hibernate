de5a509499c36da181d911e5a9f9270a63d0f0b0
Dazz Kaminski <kaminski.darek@icloud.com>
2020-05-17 16:08:33 +0100

    Added aspect logging

--- src/main/java/com/webcustomertracker/WebcustomertrackerApplication.java
+++ src/main/java/com/webcustomertracker/WebcustomertrackerApplication.java
@@ -1,9 +1,7 @@
 package com.webcustomertracker;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class WebcustomertrackerApplication {
